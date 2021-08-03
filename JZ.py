4e24edcb466e949cb7bddacae59379517acb083d
ruirui232 <87077165+ruirui232@users.noreply.github.com>
2021-08-03 16:54:52 +0800

    A第二次提交

--- JZ.py
+++ JZ.py
@@ -20,10 +20,10 @@ Y = D - R
 T = ["a1", "a2", "a3", "a4", "a5", "a6", "a7", "b1", "b2", "b3", "c1", "c2"]
 Xm=np.mean(X)#到了添加0刻线和阈值这一步
 
-fig=plt.figure(figsize=(8,6)) #新建画布
-ax=plt.subplot(1,1,1) #子图初始化
-ax.scatter(X,Y) #绘制散点图
-for i in range(len(Y)):
-    ax.text(X[i] , Y[i] +0.01, T[i], fontsize=12, color="r", style="italic", weight="light",
-            verticalalignment='center', horizontalalignment='right', rotation=0)  # 给散点加标签
-plt.show()
+# fig=plt.figure(figsize=(8,6)) #新建画布
+# ax=plt.subplot(1,1,1) #子图初始化
+# ax.scatter(X,Y) #绘制散点图
+# for i in range(len(Y)):
+#     ax.text(X[i] , Y[i] +0.01, T[i], fontsize=12, color="r", style="italic", weight="light",
+#             verticalalignment='center', horizontalalignment='right', rotation=0)  # 给散点加标签
+# plt.show()
